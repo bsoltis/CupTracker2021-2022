735cd809c7b74d51ef1ef7d5ae3af9d5084ddaab
Brian Soltis <brian@MacBook-Pro-2.local>
2021-10-18 00:00:10 -0400

    commit before react

---
+++ data/players.js
@@ -0,0 +1,72 @@
+let players = [
+    {
+        "name": "Brian",
+        "points": 0,
+        "teams" : [
+            "PIT",
+            "TBL",
+            "NYR",
+            "NYI"
+        ]
+    },
+    {
+        "name": "Chris",
+        "points": 0,
+        "teams" : [
+            "MTL",
+            "SEA",
+            "VGK",
+            "ANA"
+        ]
+    },
+    {
+        "name": "Kyle",
+        "points": 0,
+        "teams" : [
+            "WPG",
+            "WSH",
+            "EDM",
+            "BUF"
+        ]
+    },
+    {
+        "name": "Nick",
+        "points": 0,
+        "teams" : [
+            "VAN",
+            "FLA",
+            "OTT",
+            "DAL"
+        ]
+    },
+    {
+        "name": "Jack",
+        "points": 0,
+        "teams" : [
+            "CAR",
+            "CBJ",
+            "ARI",
+            "DET"
+        ]
+    },
+    {
+        "name": "Tim",
+        "points": 0,
+        "teams" : [
+            "BOS",
+            "MIN",
+            "NSH",
+            "NJD"
+        ]
+    },
+    {
+        "name": "Matt",
+        "points": 0,
+        "teams" : [
+            "STL",
+            "SJS",
+            "COL",
+            "CHI"
+        ]
+    } 
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
+var currentCupHolder = { abbreviation: 'TBL', id: 2 }
+
 window.addEventListener('load', function () {
-    var currentCupHolder = 'TBL'
+
+    fetchTeamLogoSVGSymbols()
+
     var request = new XMLHttpRequest();
     var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var url = `https://nhl-score-api.herokuapp.com/api/scores?startDate=2021-10-12&endDate=${date}`
     request.open('GET', url, true);
 
@@ -10,32 +14,35 @@ window.addEventListener('load', function () {
         if (this.status >= 200 && this.status < 400) {
             var data = JSON.parse(this.response);
 
-            for(let i = 0 ; i < data.length; i++) {
-    
+            for (let i = 0; i < data.length; i++) {
+
                 let gamesOnDate = data[i];
                 if (gamesOnDate.games.length > 0) {
-                    for(let j = 0; j < gamesOnDate.games.length; j++) {
+                    for (let j = 0; j < gamesOnDate.games.length; j++) {
                         let game = gamesOnDate.games[j]
 
                         if (game.status.state == 'FINAL') {
-                            if (game.teams.away.abbreviation == currentCupHolder || game.teams.home.abbreviation == currentCupHolder) {
-                                var otherTeam = game.teams.away.abbreviation
-                                var homeTeam = game.teams.home.abbreviation
-                                var isCurrentCupHolderHome = currentCupHolder == homeTeam
+                            if (game.teams.away.abbreviation == currentCupHolder.abbreviation || game.teams.home.abbreviation == currentCupHolder.abbreviation) {
+                                var otherTeam = game.teams.away
+                                var homeTeam = game.teams.home
+                                var isCurrentCupHolderHome = currentCupHolder.abbreviation == homeTeam.abbreviation
                                 if (!isCurrentCupHolderHome) {
-                                    otherTeam = game.teams.home.abbreviation
+                                    otherTeam = game.teams.home
                                 }
 
-                                if (game.scores[currentCupHolder] < game.scores[otherTeam]) {
+                                if (game.scores[currentCupHolder.abbreviation] < game.scores[otherTeam.abbreviation]) {
+                                    // cup switches
                                     currentCupHolder = otherTeam
+                                    var team = players.find(findTeamInPlayers)
+                                    team.points = team.points + 1
                                 }
                             }
                         }
                     }
                 }
             }
-
-            document.getElementById('currentCupHolder').innerHTML = `Current Cup Holder: ${currentCupHolder}`;
+            document.getElementById('teamName').innerText = `${currentCupHolder.locationName} ${currentCupHolder.teamName}`
+            document.getElementById('currentCupHolderImg').innerHTML = renderTeamLogo(currentCupHolder.id).outerHTML;
         } else {
             // We reached our target server, but it returned an error
 
@@ -48,3 +55,43 @@ window.addEventListener('load', function () {
 
     request.send();
 })
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../data/players.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(JSON.parse(xobj.responseText));
+        }
+    };
+    xobj.send(null);
+}
+
+function findTeamInPlayers(player) {
+    return player.teams.includes(currentCupHolder.abbreviation)
+}
+
+function fetchTeamLogoSVGSymbols() {
+    // Adapted from https://css-tricks.com/ajaxing-svg-sprite/
+    const ajax = new XMLHttpRequest();
+    ajax.open(
+        'GET',
+        'https://www-league.nhlstatic.com/images/logos/team-sprites/20202021.svg',
+        true
+    );
+    ajax.send();
+    ajax.onload = () => {
+        const div = document.createElement('div');
+        div.style.display = 'none';
+        div.innerHTML = ajax.responseText;
+        document.body.insertBefore(div, document.body.childNodes[0]);
+    };
+}
+
+function renderTeamLogo(teamId) {
+    let span = document.createElement('span')
+    span.className = 'team-logo'
+    span.innerHTML = `<svg class="team-logo__image"><use href="#team-${teamId}-20202021-dark"></use></svg>`
+    return span
+}
